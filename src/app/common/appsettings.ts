9e52ebc108397d24b17e120c16c105cae8759883
Aracely Pezua <arapezua96@gmail.com>
2020-10-05 09:51:41 -0500

    1er cambio

--- src/app/common/appsettings.ts
+++ src/app/common/appsettings.ts
@@ -1,5 +1,4 @@
-export class AppSettings {
-    //public static URL='http://35.184.146.235:3300/api/';
+export class AppSettings {//Hola
     public static URL = 'http://localhost:3000/api/';
     public static SERVICE_NO_CONECT_SERVER='Ocurrio un error al conectarse con el servidor';
     public static FILE_FORMAT_FAIL='El archivo no tiene el formato correcto';
