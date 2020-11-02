52829edb0d427088cf70867259699623057c4c3a
Aracely Pezua <arapezua96@gmail.com>
2020-11-02 10:23:43 -0500

    Soporte tecnico

--- src/app/component/soportetecnicoeditar/soportetecnicoeditar.component.ts
+++ src/app/component/soportetecnicoeditar/soportetecnicoeditar.component.ts
@@ -1,15 +1,112 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit
+} from '@angular/core';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatSnackBar
+} from '@angular/material';
+import {
+  BaseComponent
+} from '../base/base.component';
+import {
+  Router
+} from '@angular/router';
+import {
+  AppSettings
+} from '../../common/appsettings';
+
+import {
+  SoporteTecnicos,
+  SoportesTecnicosEditar
+} from '../../interface/soportetecnico.interface';
+import {
+  ResultadoApi
+} from '../../interface/common.interface';
+
+import {
+  GeneralService
+} from '../../service/general.service';
+
 
 @Component({
   selector: 'app-soportetecnicoeditar',
   templateUrl: './soportetecnicoeditar.component.html',
-  styleUrls: ['./soportetecnicoeditar.component.css']
+  styleUrls: ['./soportetecnicoeditar.component.css'],
+  providers: [GeneralService]
 })
-export class SoportetecnicoeditarComponent implements OnInit {
+export class SoportetecnicoeditarComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  soportestecnicos=[];
+
+  soportetecnico: SoporteTecnicos;
+
+  editar: boolean;
+
+  identidad = 0;
+
+
+  constructor(public dialogRef: MatDialogRef < SoportetecnicoeditarComponent >,
+      private _general_services: GeneralService,
+      @Inject(MAT_DIALOG_DATA) public data: SoportesTecnicosEditar,
+      public _router: Router,
+      public snackBar: MatSnackBar) {
+  super(snackBar, _router);
+  }
 
   ngOnInit() {
+    if (this.data.soportetecnico == null) {
+      this.editar = false;
+      this.soportetecnico = {
+        id_soportetecnico: 0,
+        nombres: '',
+        apellidos: '',
+        dni: '',
+        telefono: '',
+        correo: ''
+      };
+      this.identidad = 0;
+    } else {
+      this.editar = true;
+      this.soportetecnico = this.data.soportetecnico;
+    }
   }
+
+  AlertaGuardadoElemento( newForm ) {
+    // Preguntamos si desea Guardar el Registro
+    const mensaje = confirm('¿Te gustaría Guardar el Soporte Técnico?');
+    // Detectamos si el usuario acepto el mensaje
+    if (mensaje) {
+      this.guardar(newForm);
+      this.openSnackBar('Soporte Técnico Guardado', 99);
+    }
+  }
+  guardar( newForm ) {
+    this._general_services.saveDocente(this.soportetecnico, this.getToken().token).subscribe(
+      result => {
+        try {
+          if (result.estado) {
+            this.dialogRef.close({
+              flag: true,
+              data: this.soportetecnico
+            });
+          } else {
+            this.openSnackBar(result.mensaje, 99);
+          }
+        } catch (error) {
+          this.openSnackBar(AppSettings.SERVICE_NO_CONECT_SERVER, 99);
+        }
+      }, error => {
+        console.error(error);
+        try {
+          this.openSnackBar(error.error.Detail, error.error.StatusCode);
+        } catch (error) {
+          this.openSnackBar(AppSettings.SERVICE_NO_CONECT_SERVER, 99);
+        }
+      });
+  }
+
 
 }
--- src/app/interface/soportetecnico.interface.ts
+++ src/app/interface/soportetecnico.interface.ts
@@ -1,13 +1,10 @@
 export interface SoporteTecnicos {
-    id_docente: number;
+    id_soportetecnico: number;
     nombres: string;
     apellidos: string;
     dni: string;
     telefono: string;
     correo: string;
-    condicion: string;
-    regimen: string;
-    categoria: string;
 }
 
 export interface SoportesTecnicosEditar{
